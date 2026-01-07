7f13181f2827d9fbd9a13249dcd8f7777dfad5b0
5x5cube <db5526@naver.com>
2026-01-07 17:07:21 +0900

    테스트

---
+++ src/main/java/com/springbootstudy/app/TeamprojectminiApplication.java
@@ -0,0 +1,13 @@
+package com.springbootstudy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamprojectminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamprojectminiApplication.class, args);
+	}
+
+}
